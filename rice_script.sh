7127da3958640079ef553be69111fd82f5822d6e
rarafael <rafaelmrpch@hotmail.com>
2022-01-04 19:46:25 -0300

    New setup script, font and mako config 	new file:   .config/mako/config 	new file:   rice_script.sh 	deleted:    setup.sh 	new file:   terminal_font/Px437_IBM_VGA_8x16.ttf 	new file:   terminal_font/notice

---
+++ rice_script.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+WHITE='\033[1;37m' ; GREEN='\033[0;32m' ; NOCOLOR='\033[0m'
+printf "\n${WHITE}Setting up vim...\n"
+printf "Installing packages...\n${NOCOLOR}" ; pacman -S --noconfirm --quiet --needed vim alacritty git sway zsh zsh-syntax-highlighting sway swayidle swaylock mako waybar ttf-joypixels ranger
+git clone https://github.com/rarafael/configs "$HOME/Git/configs"
+printf "\n${WHITE}Making links...${NOCOLOR}"
+ln -s /usr/bin/vim /usr/bin/vi
+ln -sf "$HOME/Git/configs/.config/alacritty" "$HOME/.config/alacritty"
+ln -sf "$HOME/Git/configs/.config/ranger" "$HOME/.config/ranger"
+ln -sf "$HOME/Git/configs/.vimrc" "$HOME/.vimrc"
+ln -sf "$HOME/Git/configs/.zprofile" "$HOME/.zprofile"
+ln -sf "$HOME/Git/configs/.config/zsh" "$HOME/.config/zsh" ; chsh -s /usr/bin/zsh
+ln -sf "$HOME/Git/configs/.config/sway" "$HOME/.config/sway"
+ln -sf "$HOME/Git/configs/.config/mako" "$HOME/.config/mako"
+ln -sf "$HOME/Git/configs/.config/waybar" "$HOME/.config/waybar"
+cp -v "$HOME/Git/configs/terminal_font/Px_437_IBM_VGA_8x16.ttf" "$HOME/.local/share/fonts/Px_437_IBM_VGA_8x16.ttf"
+printf "\n${GREEN}Installation succesfull${NOCOLOR}\n"
+printf "${WHITE}Rebooting in 6 seconds...\n" sleep 6 && reboot
